331ab1db0cecd9450dc6e3eebc1d0e3d579bcd3d
SoniaPunjabi <64728477+SoniaPunjabi@users.noreply.github.com>
2022-04-21 14:08:46 +0530

    Add files via upload

---
+++ chat/server/chat.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"bufio"
+	pb "chat_service/chat/proto"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func (s *Server) Chat(stream pb.ChatService_ChatServer) error {
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		fmt.Printf("Allen-> %s", req.Message)
+		if err != nil {
+			log.Fatalf("error while reading client stream: %v\n", err)
+		}
+		inputReader := bufio.NewReader(os.Stdin)
+		input, _ := inputReader.ReadString('\n')
+		err = stream.Send(&pb.ChatResponse{
+			Response: input,
+		})
+		if err != nil {
+			log.Fatalf("error while sending data to client stream: %v\n", err)
+		}
+	}
+}
